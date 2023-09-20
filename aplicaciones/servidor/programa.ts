cc14c8fba951d2fbc35beef35f23567dcb1f86a3
anattolia <bustamanter.antonia@gmail.com>
2023-09-20 17:26:15 -0500

    🦬 Base del proyecto con trasero y frente

--- aplicaciones/servidor/programa.ts
+++ aplicaciones/servidor/programa.ts
@@ -5,6 +5,7 @@ import Image from './utilidades/Image.js';
 import { createCanvas } from 'canvas';
 import { createWriteStream } from 'fs';
 import * as url from 'url';
+import { resolve } from 'path';
 const __filename = url.fileURLToPath(import.meta.url);
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 
@@ -33,39 +34,17 @@ async function inicio() {
 (           )
 ( (  )   (  ) )
 (__(__)___(__)__)`;
-    // impresora.align('ct').espacioEntreLinea(0).text(gato);
-    // impresora.cut().desconectar();
-    // impresora.desconectar();
-    // const lienzo = createCanvas(430, 300);
-    // const ctx = lienzo.getContext('2d');
-    // ctx.rect(2, 2, 410, 200);
-    // ctx.stroke();
-    // // ctx.strokeRect(0, 0, 450, 300);
-    // const out = createWriteStream(__dirname + '/test.png');
-    // const stream = lienzo.createPNGStream();
-    // stream.pipe(out);
-    // out.on('finish', async () => {
-    //   console.log('The PNG file was created.');
-    //   const foto = await Image.load('./test.png');
 
-    //   impresora
-    //     .align('ct')
-    //     .image(foto, 'd24')
-    //     .then(() => {
-    //       impresora.cut().desconectar();
-    //     });
-    // });
-    // impresora.desconectar();
-    // const cuadro = lienzo.toDataURL();
+    // Imprimir los dibujitos de arriba
+    impresora.align('ct').espacioEntreLinea(0).text(vaquita);
+    impresora.cut().desconectar();
 
-    // const foto = await Image.load(cuadro);
-    // console.log(foto);
-    impresora
-      .align('ct')
-      .image(foto, 'd24')
-      .then(() => {
-        impresora.cut().desconectar();
-      });
+    // impresora
+    //   .align('ct')
+    //   .image(foto, 'd24')
+    //   .then(() => {
+    //     impresora.cut().desconectar();
+    //   });
   }
 
   function escribir(impresora: OutEndpoint, datos: Buffer) {
